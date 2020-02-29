1f9daf27916b328281eafb994b8e1ba826762b42
napori0624 <naporin0624@gmail.com>
2020-03-01 02:50:47 +0900

    :shower: fix build passed

--- src/pages/_document/css.tsx
+++
@@ -1,9 +0,0 @@
-import React, { Fragment } from 'react';
-
-export function Css() {
-  return (
-    <Fragment>
-      <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/reset-css@5.0.1/reset.min.css"></link>
-    </Fragment>
-  );
-}
--- src/pages/_document/script.tsx
+++
@@ -1,9 +0,0 @@
-import React, { Fragment } from 'react';
-
-export function Script() {
-  return (
-    <Fragment>
-      <script async src="https://platform.twitter.com/widgets.js"></script>
-    </Fragment>
-  );
-}
