5b9bcfa0d6227a371b24bf792f12ecb3bf20e93f
euxn23 <5271091+euxn23@users.noreply.github.com>
2020-02-29 23:44:42 +0900

    chore: use src dir (#1)
    * chore: use src dir
    
    * :bug: fix directory
    
    Co-authored-by: naporitan <32933709+naporin0624@users.noreply.github.com>

--- src/components/UpdateLog/index.tsx
+++ src/components/UpdateLog/index.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Log } from '../sharted/Log';
+import { Log } from '../sharted';
 
 export function UpdateLog(props: any) {
   return <Log title={'更新履歴'} {...props} />;
