3eea94b199cc8158c33cd615f0b717a9e3adea95
napori0624 <naporin0624@gmail.com>
2020-03-01 00:04:38 +0900

    :+1: create fixed header

--- src/components/FixedHeader/index.tsx
+++ src/components/FixedHeader/index.tsx
@@ -1,5 +1,11 @@
 import React from 'react';
+import { Container, Button, Link } from './styles';
 
 export function FixedHeader() {
-  return <div />;
+  return (
+    <Container>
+      <Button>とめる？</Button>
+      <Link href="#">配信の一覧へ</Link>
+    </Container>
+  );
 }
