e8b9c91415d3aeeabc52d8d31d4babd3ded5a95e
napori0624 <naporin0624@gmail.com>
2020-03-02 10:15:38 +0900

    :+1: add sort

--- src/components/UpdateLog/enhance.ts
+++ src/components/UpdateLog/enhance.ts
@@ -1,26 +1,30 @@
-import { Props } from '.';
+import { Props, Log } from '.';
 import { useMemo } from 'react';
-import { isEqual, format } from 'date-fns';
+import { format } from 'date-fns';
 
 export function useEnhance(props: Props) {
   const createLogs = useMemo(
     () =>
       props.logs
-        .filter((log) => isEqual(log.createdAt, log.updatedAt))
+        .filter((log) => !log.updatedAt)
         .map((log) => ({
           ...log,
           date: format(log.createdAt, 'yyyy/MM/dd'),
-        })),
+        }))
+        .sort((log1, log2) => (log1.createdAt < log2.createdAt ? 1 : -1))
+        .slice(0, 7),
     [props.logs],
   );
   const updateLogs = useMemo(
     () =>
       props.logs
-        .filter((log) => !isEqual(log.createdAt, log.updatedAt))
+        .filter((log): log is Required<Log> => !!log.updatedAt)
         .map((log) => ({
           ...log,
           date: format(log.updatedAt, 'yyyy/MM/dd'),
-        })),
+        }))
+        .sort((log1, log2) => (log1.updatedAt < log2.updatedAt ? 1 : -1))
+        .slice(0, 7),
     [props.logs],
   );
 
