81c4a1830e1508eb683b4e275391a32c47535a58
ndxbn <sitri.ndxbn@gmail.com>
2020-03-11 08:17:49 +0900

    Update index.ts

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,4 +1,4 @@
-export const endpoint = 'https://sanabutton.github.io';
+export const endpoint = 'https://www.natorisana.love';
 export const endpointV1 = `${endpoint}/api/v1`;
 export const endpointSound = `${endpoint}/sounds`;
-export const host = 'https://www.natorisana.love';
+export const host = 'https://devel.natorisana.love';
